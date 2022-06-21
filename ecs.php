388645deac81f547552a7c41276a1200aadbd4ae
Anatoly Kochetkov <yolar.sh@gmail.com>
2022-06-21 16:18:41 +0400

    init

---
+++ ecs.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Fixer\ArrayNotation\ArraySyntaxFixer;
+use PhpCsFixer\Fixer\ControlStructure\NoSuperfluousElseifFixer;
+use PhpCsFixer\Fixer\ControlStructure\NoUselessElseFixer;
+use PhpCsFixer\Fixer\ReturnNotation\NoUselessReturnFixer;
+use PhpCsFixer\Fixer\Whitespace\NoExtraBlankLinesFixer;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
+
+return static function (ECSConfig $ecsConfig): void {
+    $ecsConfig->paths([__DIR__ . '/src', __DIR__ . '/tests']);
+
+    $ecsConfig->rules([
+        NoUselessElseFixer::class,
+        NoUselessReturnFixer::class,
+        NoSuperfluousElseifFixer::class,
+        NoExtraBlankLinesFixer::class
+    ]);
+
+    $ecsConfig->ruleWithConfiguration(ArraySyntaxFixer::class, [
+        'syntax' => 'short',
+    ]);
+
+    $ecsConfig->sets([
+        SetList::PSR_12,
+    ]);
+};
