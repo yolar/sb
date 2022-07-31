3a1357cb60689a76b649ad00060e490964968ca1
Anatoly Kochetkov <yolar.sh@gmail.com>
2022-07-31 21:27:21 +0400

    hw 3

---
+++ src/LogCommand.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+class LogCommand implements CommandInterface
+{
+    public function __construct(
+        private string $message,
+        private string $path = 'php://stdout',
+    ) {
+    }
+
+    public function execute(): void
+    {
+        $file = new \SplFileObject($this->path, 'a');
+        $file->fwrite(\sprintf('[%s] - %s%s', date('d-m-y h:i:s'), $this->message, PHP_EOL));
+    }
+}
