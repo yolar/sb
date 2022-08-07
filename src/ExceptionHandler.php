e4e4470ab333344ed791c639d87cb3be6d07787c
yolar <yolar.sh@gmail.com>
2022-08-07 12:52:28 +0400

    hw 3

---
+++ src/ExceptionHandler.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App;
+
+use Ds\Queue;
+
+class ExceptionHandler
+{
+    /**
+     * @var string[]
+     */
+    private array $strategies = [
+        FirstRetryCommand::class => 'onFirstRetry',
+        SecondRetryCommand::class => 'onSecondRetry',
+    ];
+
+    public function __construct(
+        private Queue $queue,
+    ) {
+    }
+
+    public function handle(CommandInterface $command, \Throwable $throwable): void
+    {
+        $handler = $this->strategies[$command::class] ?? 'onException';
+
+        $this->$handler($command, $throwable);
+    }
+
+    private function onException(CommandInterface $command, \Throwable $_throwable): void
+    {
+        $this->queue->push(new FirstRetryCommand($command));
+    }
+
+    private function onFirstRetry(CommandInterface $command, \Throwable $_throwable): void
+    {
+        $this->queue->push(new SecondRetryCommand($command));
+    }
+
+    private function onSecondRetry(CommandInterface $_command, \Throwable $throwable): void
+    {
+        $this->queue->push(new LogCommand($throwable->getMessage()));
+    }
+}
---
+++ src/FirstRetryCommand.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+class FirstRetryCommand implements CommandInterface
+{
+    public function __construct(
+        private CommandInterface $command,
+    ) {
+    }
+
+    public function execute(): void
+    {
+        $this->command->execute();
+    }
+}
